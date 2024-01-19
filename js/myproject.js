b430375cdba4ff9e4ba2dfdd995d25a863ef6dde
Sandeep Samanta <148564577+sunnyboy7886@users.noreply.github.com>
2024-01-19 21:54:58 +0530

    Add files via upload

---
+++ js/myproject.js
@@ -0,0 +1,38 @@
+function bgvisible1() {
+    let bg1 = document.getElementById("bg-card1");
+    bg1.style.background = "rgb(94, 13, 170)";
+}
+function bgvisible2() {
+    let bg2 = document.getElementById("bg-card2");
+    bg2.style.background = "rgb(94, 13, 170)";
+}
+
+function bgremove1() {
+    let bg = document.getElementById("bg-card1");
+    bg.style.background = "white";
+}
+    
+function bgremove2() {
+    let bg = document.getElementById("bg-card2");
+    bg.style.background = "white";
+}
+
+function bgvisible3() {
+    let bg = document.getElementById("bg-card3");
+    bg.style.background = "rgb(94, 13, 170)";
+}
+
+function bgremove3() {
+    let bg = document.getElementById("bg-card3");
+    bg.style.background = "white";
+}
+
+function bgvisible4() {
+    let bg = document.getElementById("bg-card4");
+    bg.style.background = "rgb(94, 13, 170)";
+}
+
+function bgremove4() {
+    let bg = document.getElementById("bg-card4");
+    bg.style.background = "white";
+}
